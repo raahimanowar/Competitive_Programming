f732ce00b9761b907c885cf63be74186564031b0
raahimanowar <raahimfardin@gmail.com>
2026-04-06 00:20:36 +0600

    add K L in sheet 5

---
+++ NC Sheet 5/K.cpp
@@ -0,0 +1,26 @@
+
+using namespace std;
+
+void shiftRight(vector<int> &a, int x) {
+    int n = a.size();
+    x = x % n;
+
+    rotate(a.rbegin(), a.rbegin() + x, a.rend());
+}
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    shiftRight(a, x);
+
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
---
+++ NC Sheet 5/L.cpp
@@ -0,0 +1,31 @@
+
+using namespace std;
+
+void concate(vector<int>& a, vector<int>& b) {
+    vector<int> c;
+
+    c.insert(c.end(), b.begin(), b.end());
+    c.insert(c.end(), a.begin(), a.end());
+
+    for(int i : c) {
+        cout << i << " ";
+    }
+}
+
+int main() {
+    int n, x;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+
+    concate(a, b);
+    return 0;
+}
